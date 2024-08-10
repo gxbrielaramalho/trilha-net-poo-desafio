fc88bee8d3156a4304eea203466bb389c5a55486
Gabriela Ramalho <gabrielathecreator@gmail.com>
2024-08-09 22:30:29 -0300

    Desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Smartphone iphone = new Iphone("123456789", "iPhone 13", "ABC123", 128);
+        iphone.Ligar();
+        iphone.InstalarAplicativo("WhatsApp");
+
+        Smartphone nokia = new Nokia("987654321", "Nokia 3310", "DEF456", 16);
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Snake");
+    }
+}
